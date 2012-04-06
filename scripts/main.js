42c974f049490cf92444382c4ca0eef8274bd35b
Nikhil Suresh <nklsrh@gmail.com>
2012-04-06 11:57:08 +1000

    install button

--- scripts/main.js
+++ scripts/main.js
@@ -10,4 +10,7 @@ var FPS = 30;
 var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext('2d');
 
-
+function Install(){
+        navigator.mozApps.install("./manifest.json");
+        document.getElementById('installBtn').style.display = "none";        
+}
