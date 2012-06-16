91c3a4ee1473235a3256a62eacf6fd477f0b8dc1
Nikhil Suresh <nklsrh@github.com>
2012-06-16 22:19:14 +1000

    linked CANVAS_WIDTH to actual canvas.width
    dunno why i didnt do that before??

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,9 @@ function Main(){
 	game.Draw();
 }
 
-var FPS = 30;
+var FPS = 60;
 var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext('2d');
 var CANVAS_HEIGHT = 480, CANVAS_WIDTH = 800;
+canvas.height = CANVAS_HEIGHT;
+canvas.width = CANVAS_WIDTH;
